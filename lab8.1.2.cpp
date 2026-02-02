065eb50076e955a79f26cd9aed5a0f49cd62a050
650462636292 <153263126+6504062636292@users.noreply.github.com>
2026-02-02 18:05:50 +0700

    Add function to increment values in an array

--- lab8.1.2.cpp
+++ lab8.1.2.cpp
@@ -3,12 +3,15 @@
 int findMax(int num[], int size);
 void printAr(int myAr[], int size);
 void addone(int number[], int add[], int sizeNum);
+void addtwo(int number[], int pos[], int x);
+
 
 int main() {
     int number[5] = {22, 50, 100, 99, 9};
     int score[10] = {20, 56, 10, 96, 5, 5, 30, 39, 24, 9};
     int n1[5] = {1, 2, 3, 4, 5};
-    int max;
+    int pos[5];
+    int max,x;
 
     max = findMax(number, 5);
     printf("%d\n", max);
@@ -16,12 +19,24 @@ int main() {
     printAr(number,5);
     addone(number, score, 5);
     
+    for(int i=0;i<5;i++){
+    	scanf("%d",&pos[i]);
+	}
+	scanf("%d",&x);
+	addtwo(number,pos,x);
+
     printf("\n");
     printAr(number, 5);
     
     printf("Maximum of these number is %d\n", max);
     
     return 0;
+}
+void addtwo(int number[], int pos[], int x){
+	int i;
+	for(i=0;i<5;i++){
+		pos[i] += x;
+	}
 }
 
 void addone(int number[], int add[], int sizeNum) {
